856c0cc9f1016e850b74f02bbc0997815f1b82aa
Shabbir Ahamed <shabbirkimgbrand55@gmail.com>
2025-01-08 00:14:59 +0600

    data set

--- src/Layouts/AllCatagory/AllCatagory.jsx
+++ src/Layouts/AllCatagory/AllCatagory.jsx
@@ -1,60 +1,45 @@
-import '../../Coustom-styles/AllCatagoryCss/AllCatagory.css'
+import '../../Coustom-styles/AllCatagoryCss/AllCatagory.css';
 import { Link, useLoaderData } from 'react-router-dom';
 
 const AllCatagory = () => {
-
-    const allDeta = useLoaderData();
-    const {
-        _id,
-        others_info: { is_todays_pick, is_trending },
-        category_id,
-        rating: { number: ratingNumber, badge: ratingBadge },
-        total_view,
-        title,
-        author: { name: authorName, published_date, img: authorImg },
-        thumbnail_url,
-        image_url,
-        details
-    } = useLoaderData();
-
+    const UserData = useLoaderData();
+    console.log(UserData);
 
     return (
         <div>
-            <div>
-                <ul className="text-2xl text-black font-bold">
-                    All Category
-                    <div className="ml-2.5 text-lg font-medium mt-5 text-center">
-                        <li className="ml-2 text-color p-4 mr-40 text-gray-700 rounded-md">
-                            <Link to="/breaking-news">Breaking News</Link>
-                        </li>
-                        <li className="ml-2 p-4 mr-40 text-gray-700 rounded-md">
-                            <Link to="/national-news">National News</Link>
-                        </li>
-                        <li className="ml-2 p-4 mr-40 text-gray-700 rounded-md">
-                            <Link to="/regular-news">Regular News</Link>
-                        </li>
-                        <li className="ml-2 p-4 mr-40 text-gray-700 rounded-md">
-                            <Link to="/international-news">International News</Link>
-                        </li>
-                        <li className="ml-2 p-4 mr-40 text-gray-700 rounded-md">
-                            <Link to="/sports">Sports</Link>
-                        </li>
-                        <li className="ml-2 p-4 mr-40 text-gray-700 rounded-md">
-                            <Link to="/entertainment">Entertainment</Link>
-                        </li>
-                        <li className="ml-2 p-4 mr-40 text-gray-700 rounded-md">
-                            <Link to="/culture">Culture</Link>
-                        </li>
-                        <li className="ml-2 p-4 mr-40 text-gray-700 rounded-md">
-                            <Link to="/arts">Arts</Link>
-                        </li>
-                        <li className="ml-2 p-4 mr-40 text-gray-700 rounded-md">
-                            <Link to="/all-news">All News</Link>
-                        </li>
-                    </div>
-                </ul>
-
-            </div>
+            <ul className="text-2xl text-black font-bold">
+                All Categories
+                <div className="ml-2.5 text-lg font-medium mt-5 text-center">
+                    <li className="ml-2 text-color p-4 mr-40 text-gray-700 rounded-md">
+                        <Link to="/breaking-news">Breaking News</Link>
+                    </li>
+                    <li className="ml-2 p-4 mr-40 text-gray-700 rounded-md">
+                        <Link to="/national-news">National News</Link>
+                    </li>
+                    <li className="ml-2 p-4 mr-40 text-gray-700 rounded-md">
+                        <Link to="/regular-news">Regular News</Link>
+                    </li>
+                    <li className="ml-2 p-4 mr-40 text-gray-700 rounded-md">
+                        <Link to="/international-news">International News</Link>
+                    </li>
+                    <li className="ml-2 p-4 mr-40 text-gray-700 rounded-md">
+                        <Link to="/sports">Sports</Link>
+                    </li>
+                    <li className="ml-2 p-4 mr-40 text-gray-700 rounded-md">
+                        <Link to="/entertainment">Entertainment</Link>
+                    </li>
+                    <li className="ml-2 p-4 mr-40 text-gray-700 rounded-md">
+                        <Link to="/culture">Culture</Link>
+                    </li>
+                    <li className="ml-2 p-4 mr-40 text-gray-700 rounded-md">
+                        <Link to="/arts">Arts</Link>
+                    </li>
+                    <li className="ml-2 p-4 mr-40 text-gray-700 rounded-md">
+                        <Link to="/all-news">All News</Link>
+                    </li>
+                </div>
+            </ul>
+            <h1>nothing</h1>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
 import {
   createBrowserRouter,
   RouterProvider,
-} from "react-router-dom";
+} from 'react-router-dom';
 import Root from './Layouts/Root/Root.jsx';
 import Home from './Pages/Home.jsx';
-import AllCatagory from './Layouts/AllCatagory/AllCatagory.jsx';
+import AllCategory from './Layouts/AllCatagory/AllCatagory.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root></Root>,
+    element: <Root />,
     children: [
       {
         path: '/',
-        element: <Home></Home>,
-        loader: async () => {
-          const res = await fetch('/DragonNews/news.json');
-          return res.json();
+        element: <Home />,
+        loader: () => {
+          fetch('../src/Deta/news.json')
+            .then(res => res.json())
+            .then(data => {
+              console.log(data);
+            })
         }
-      }
+      },
     ]
   },
 ]);
@@ -29,5 +32,5 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
