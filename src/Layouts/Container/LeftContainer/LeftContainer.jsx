cbf7c892de11f6d53928bafedb91664632f0efa3
Shabbir Ahamed <shabbirkimgbrand55@gmail.com>
2025-01-08 12:13:10 +0600

    left side logging handle

--- src/Layouts/Container/LeftContainer/LeftContainer.jsx
+++ src/Layouts/Container/LeftContainer/LeftContainer.jsx
@@ -1,8 +1,34 @@
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
 
 const LeftContainer = () => {
     return (
         <div>
-            <h1 className="text-2xl text-red-600">its container</h1>
+            <div>
+                <h1 className="text-2xl text-black font-bold ml-2">Login Wtih</h1>
+                <div className="mt-5">
+                    <div className=" text-blue-600 border-blue-700 p-2  border
+                     mt-2 w-full rounded-lg flex justify-between items-center">
+                        <FaFacebook className="text-2xl"></FaFacebook>
+                        <button>Login with Facebook</button>
+                        <br />
+                    </div>
+                    <div className=" text-black border-gray-700 p-2  border
+                     mt-2 w-full rounded-lg flex justify-between items-center">
+                        <FaGithub className="text-2xl"></FaGithub>
+                        <button>Login with Github</button>
+                        <br />
+                    </div>
+
+                    <div className=" text-red-600 border-red-700 p-2  border
+                     mt-2 w-full rounded-lg flex justify-between items-center">
+                        <FcGoogle className="text-2xl"></FcGoogle>
+                        <button >Login with Google</button>
+                        <br />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
